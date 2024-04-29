c244ad2341372316e15c034cd7a2d0a6cbbab646
EllyoottO <166592232+EllyoottO@users.noreply.github.com>
2024-04-29 18:56:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,37 @@
 fetch("instrumentsMusiques.json")
   // Une fois que la réponse est reçue
   .then(rep => {
-    // transforme la reponse obtenu en JSON
+    // transforme la reponse obtenu en JSON 
     return rep.json();
   })
   // Une fois que les données JSON sont extraites avec succès, jeles transmet à une fonction pour l'utiliser plus tard
   .then(data => {
     // Appelle la fonction addProducts() avec les données JSON en entrée
-    addProducts(data);
+    addContent(data);
   });
+
+
+
+
 // Cette fonction prend les données JSON en entrée et effectue une action sur celles-ci
-function addProducts(data) {
+function addContent(data) {
   // Affiche les données dans la console du navigateur
-  console.log(data);}
+  console.log(data)
+  console.log(data.entreprise.nomCommercial);
+  console.log(data.entreprise.phraseAccroche)
+  console.log(data.entreprise.texteAppelAction)
+  console.log(data.entreprise.avantagesClients)
 
-  let avantage =""
-  data.avantagesClients.forEach(element => {
-    avantage = data.avantagesClients
+  let nom = data.entreprise.nomCommercial;
+  let phraseAccroche=data.entreprise.phraseAccroche;
+  let appelAction = data.entreprise.texteAppelAction;
+  let avantages = data.entreprise.avantagesClients;
 
-    let Prod =""
-    data.produits.forEach(element => {
-        prod+=`<img src="${element.image}"> <p> ${element.nom}</p> <p>${element.description}</p> `
-    });
+  avantages.forEach(element => {
+    console.log(element)
+    
   });
 
-  document.querySelector("#content").innerHTML += avantage
+
+
+}
